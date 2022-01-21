0529ea30d4c7ce2cfc340361ffe85bb105d33f6a
Trendon Cato <trendoncato@hotmail.com>
2022-01-21 11:37:12 +0800

    Add Discount Grpc

---
+++ src/Services/Discount/Discount.Grpc/MapperProfiles/IMapperInitialize.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.MapperProfiles
+{
+    public class IMapperInitialize :Profile
+    {
+        public IMapperInitialize()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
